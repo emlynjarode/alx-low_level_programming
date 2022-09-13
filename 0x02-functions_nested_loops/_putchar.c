09f4d7941bed9fca916ee6e5cbbb7363ecb60ea5
emlynjarode <emlynat02@gmail.com>
2022-09-13 12:59:50 -0700

    addition

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - char type
+ * Description - prints character to output
+ *
+ * Return: always 0
+ */
+char _putchar(char a)
+{
+	return (a);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,8 @@
  *
  * Return: 
  */
-char _putchar(char );
+char _putchar(char);
 
 void print_alphabet(void);
+
+void print_alphabet_x10(void);
