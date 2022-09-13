3780a17960ee5dc6b9ed592fb1d894218ba15e87
emlynjarode <emlynat02@gmail.com>
2022-09-13 10:33:40 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - prints _putchar followed by a new line
+ *
+ * return: (success)
+ *
+ */
+void main(void)
+{
+	putchar("_putchar");
+	putchar("\n");
+
+	return (0);
+}
