d4b7814561f47c6e0a7e886aadb7ce487693150d
emlynjarode <emlynat02@gmail.com>
2022-09-13 19:49:55 -0700

    9,10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - int return type
+ * Description - adds 2 values
+ * @num1: first parameter
+ * @num2: second parameter
+ *
+ * Return: integer value
+ */
+int add(int num1, int num2)
+{
+
+	return (num1 + num2);
+}
+
