d2af0f3332b1114300e0754e07362ee5aaf642c9
emlynjarode <emlynat02@gmail.com>
2022-09-14 06:50:33 -0700

    Multiple mains

--- 0x02-functions_nested_loops/0-main.c
+++ 0x02-functions_nested_loops/0-main.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is posotuve or negative 
+ * main - tests function that prints if integer is posotuve or negative
  * Return: always 0
  */
-int main (void)
+int main(void)
 {
 	int i = 0;
+
 	positive_or_negative(i);
 
 	return (0);
