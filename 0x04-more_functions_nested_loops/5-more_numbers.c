6249e556b13c972dab7c9b334b75029e66e8ef18
emlynjarode <emlynat02@gmail.com>
2022-09-17 02:02:46 -0700

    try

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,7 @@ void more_numbers(void)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			if (y > 9)
-			{
-				_putchar((y / 10) + '0');
-			}
-			_putchar((y % 10) + '0');
+			_putchar(b);
 		}
 
 		_putchar('\n');
