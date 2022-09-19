3b23fc3559a21405488f8bc7cdab06b90b3edbf9
emlynjarode <emlynat02@gmail.com>
2022-09-19 14:07:21 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcpy - return type char
+ * Description - copies the string pointed to by src
+ * @dest: buffer to paste string
+ * @src : string to be copied
+ *
+ * Return: *dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int cows;
+
+	for (cows = 0; src[cows] != '\0'; cows++)
+	{
+		dest[cows] = src[cows];
+	}
+	dest[cows++] = '\0';
+	return (*dest);
+}
