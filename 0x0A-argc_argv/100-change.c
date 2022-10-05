fb25e6292dd279e3292162a27424af3d3ef0a009
emlynjarode <emlynat02@gmail.com>
2022-10-05 11:54:54 -0700

    0-5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -  prints the minimum number of coins to make change
+ * @argc - argument count
+ * @argv - argument count
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
